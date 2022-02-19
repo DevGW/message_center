fc4a73aff6f6cab5360efdb96060384f2fd5a11c
Jason Becht <jason@cryptechstudios.com>
2022-02-19 14:08:20 -0600

    .......

--- config/initializers/message_center.rb
+++ config/initializers/message_center.rb
@@ -9,7 +9,7 @@ MessageCenter.setup do |config|
     config.table_data_prefix = "dgw_mc_"
 
         puts "user class is : "
-        puts self.user_class
+        puts config.user_class
         puts %Q(
             #############################################
             #############################################
